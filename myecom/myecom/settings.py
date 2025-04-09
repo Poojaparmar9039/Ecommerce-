7059bcfc281a02f7b9c775b6a332580df98376d4
Poojaparmar9039 <157222585+Poojaparmar9039@users.noreply.github.com>
2025-04-09 13:50:12 +0530

    Update settings.py

--- myecom/myecom/settings.py
+++ myecom/myecom/settings.py
@@ -81,11 +81,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecom',
         'USER': 'root',
-        'PASSWORD': '123456@pooja',
+        'PASSWORD': '00000000',
         'HOST': '127.0.0.1',  # Use '127.0.0.1' for local development
         'PORT': '3306',  # Default MySQL port
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES     ......'"
         }
     }
 }
